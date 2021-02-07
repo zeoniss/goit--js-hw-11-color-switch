545341990bb0426527bec3fd44364d8d7b77b07c
Vladimir Kriukov <zeoniss28@gmail.com>
2021-02-07 12:50:08 +0200

    summary

--- src/js/colorChange.js
+++ src/js/colorChange.js
@@ -11,11 +11,12 @@ const refs = {
     body: document.querySelector('body'),
     startBtn: document.querySelector('[data-action="start"]'),
     stopBtn: document.querySelector('[data-action="stop"]'),
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
+
 };
+
+refs.startBtn.addEventListener('click', startChanging);
+refs.stopBtn.addEventListener('click', stopChanging);
 
 const randomColor = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -25,11 +26,6 @@ function nextColor() {
     const nextColor = randomColor(0, colors.length - 1);
     refs.body.setAttribute('style', `background-color: ${colors[nextColor]}`);
 }
-
-
-refs.startBtn.addEventListener('click', startChanging);
-refs.stopBtn.addEventListener('click', stopChanging);
-
 let intervalColor = 0;
 
 function startChanging() {
@@ -40,14 +36,5 @@ function startChanging() {
 function stopChanging() {
     clearInterval(intervalColor);
     refs.startBtn.removeAttribute('disabled');
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
 }
-refs.startBtn.addEventListener('click', startChanging);
-refs.stopBtn.addEventListener('click', stopChanging);
-=======
-}
->>>>>>> Stashed changes
-=======
-}
->>>>>>> Stashed changes
